fec53226c777c23df0c4beecc48c16dd8d9083ed
Tara Sarpoolaki <78650816+tarasap@users.noreply.github.com>
2026-08-17 10:16:21 +0330

    Change non-blocking assignments to blocking assignments

--- rtl/TERME_Engine/General_Elements/Mux_4.v
+++ rtl/TERME_Engine/General_Elements/Mux_4.v
@@ -23,13 +23,13 @@ module Mux_4 #(parameter size = 32) (
     always @(in_1,in_2,in_3,in_4,select) begin
 
         if (select == 2'b00)
-            mux_out <=  in_1;
+            mux_out  =  in_1;
         else if (select == 2'b01)
-            mux_out <= in_2;
+            mux_out  = in_2;
         else if (select == 2'b10)
-            mux_out <= in_3;
+            mux_out  = in_3;
         else   
-            mux_out <=  in_4;
+            mux_out  =  in_4;
 
 end 
 endmodule
